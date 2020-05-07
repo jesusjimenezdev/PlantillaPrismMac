c79d550816bfd868fb6dbbf866937db4e91c2e8b
jesusjimenezdev <Gsx600fx>
2020-05-07 11:52:11 +0200

    final commit

---
+++ EjemploPrismMac/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EjemploPrismMac
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("/NavigationPage/MainPage");
+        }
+    }
+}
